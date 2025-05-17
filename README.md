3c769eccd6756f8ff4c5950f5cc931d1c3ed1735
kenz1us <neicygaming@gmail.com>
2025-05-17 22:42:01 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Eno Leriand
+
+- [z0rs.github.io](https://z0rs.github.io/)
+
---
+++ gatsby-config.js
@@ -0,0 +1,87 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV || 'production'}`
+});
+
+const wrapESMPlugin = (name) =>
+  function wrapESM(opts) {
+    return async (...args) => {
+      const mod = await import(name);
+      const plugin = mod.default(opts);
+      return plugin(...args);
+    };
+  };
+
+module.exports = {
+  pathPrefix: "/neicyy.github.io",
+  // flags: {
+  //   FAST_DEV: true
+  // },
+  trailingSlash: 'always',
+  siteMetadata: {
+    name: 'Eno Leriand',
+    description: 'Security & Research',
+    keywords: [
+      'React',
+      'Gatsby',
+      'JavaScript',
+      'TypeScript',
+      'Flow',
+      'Styled Components',
+      'Theme UI',
+      'Tailwind',
+      'Jest',
+      'Enzyme',
+      'React Testing Libary',
+      'Node.js',
+      'Fauna',
+      'Jamstack',
+      'Component Library',
+      'Serverless Functions'
+    ],
+    siteUrl: process.env.URL,
+    defaultImage: '/images/76135196.jpeg'
+  },
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-postcss',
+    'gatsby-transformer-json',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        rehypePlugins: [wrapESMPlugin('rehype-slug'), [wrapESMPlugin('rehype-autolink-headings'), { behavior: 'wrap' }]]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaults: {
+          quality: 70,
+          formats: ['auto', 'webp'],
+          placeholder: 'blurred'
+        }
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/pages/`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/articles/`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/ctfs/`
+      }
+    }
+  ],
+  partytownProxiedURLs: [`https://www.googletagmanager.com/gtag/js?id=${process.env.GATSBY_GA_MEASUREMENT_ID}`]
+};
